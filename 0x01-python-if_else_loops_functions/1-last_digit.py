da1ea32af2f94a446d413f995ff168d6a648440e
Alexa <alexafdeveloper@gmail.com>
2019-04-30 23:02:56 +0000

    fixes: line more than 72 characters

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,8 @@ if digit > 5:
 	print("Last digit of {:d} is {:d} and is greater than 5"
 		.format(number, digit))
 elif digit == 0:
-	print("Last digit of {:d} is {:d} and is 0".format(number, digit))
+	print("Last digit of {:d} is {:d} and is 0"
+		.format(number, digit))
 elif digit < 6:
 	print("Last digit of {:d} is {:d} and is less than 6 and not 0"
 		.format(number, digit))
