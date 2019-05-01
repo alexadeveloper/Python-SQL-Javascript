ff89d48d3a11591c4517f49db67e19cb82f0a9d6
Alexa <alexafdeveloper@gmail.com>
2019-05-01 03:20:18 +0000

    fixed: task 12 fucntion

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 3 == 0:
-        print("Fizz", end='')
-    elif i % 5 == 0:
-        print("Buzz", end='')
-    else:
-        print("{}".format(i), end='')
-    if i != 99:
-        print(" ", end='')
-print('')
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end='')
+        elif i % 5 == 0:
+            print("Buzz", end='')
+        else:
+            print("{}".format(i), end='')
+        if i != 99:
+            print(" ", end='')
