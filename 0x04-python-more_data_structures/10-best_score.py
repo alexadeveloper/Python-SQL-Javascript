2ba7aa56b9bded265f6595c95e8a68b6d7ee8cc6
Alexa <alexafdeveloper@gmail.com>
2019-05-09 23:42:26 +0000

    the biggest int in a dic

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    num_mayor = 0
+    for i in list(a_dictionary.keys()):
+        if a_dictionary[i] > num_mayor:
+            num_mayor = a_dictionary[i]
+            key_mayor = i
+    return key_mayor
