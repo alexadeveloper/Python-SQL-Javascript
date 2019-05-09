490da853faeb4e995dc9fe0cf901e85afb0e852e
Alexa <alexafdeveloper@gmail.com>
2019-05-09 13:59:02 +0000

    suqre simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#/usr/bin/python3
+def _pow(n):
+    return n ** 2
+def square_matrix_simple(matrix=[]):
+    nueva = []
+    for i in matrix:
+        nueva.append(list(map(lambda n: _pow(n), i)))
+    return nueva
