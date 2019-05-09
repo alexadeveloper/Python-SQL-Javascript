fde0d08bf4f249b4169f7641f77d6c08468c06dc
Alexa <alexafdeveloper@gmail.com>
2019-05-09 23:49:35 +0000

    fixes the dic empty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     num_mayor = 0
     for i in list(a_dictionary.keys()):
