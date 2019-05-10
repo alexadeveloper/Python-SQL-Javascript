1fdc884826a4efe0249df4d91b022b33beb9b979
Alexa <alexafdeveloper@gmail.com>
2019-05-10 01:27:56 +0000

    romans to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    numeros = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    entero = 0
+    letra = 'I'
+    inversa = roman_string[::-1]
+    for i in inversa:
+        if numeros[i] >= numeros[letra]:
+            letra = i
+            entero += numeros[i]
+        else:
+            entero -= numeros[i]
+    return entero
