725b06e8b07fcf1828a22305f00038a2a70aa559
Alexa <alexafdeveloper@gmail.com>
2019-05-21 23:07:06 +0000

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Doc Class Square"""
+
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """Initial size of a square
+
+           Args:
+              the size type int
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """return the current size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """change the value of size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ calculate the area"""
+
+        area = self.__size ** 2
+        return area
