89d183276c1ef4d90ef3606d9a1b034243b72c44
Alexa <alexafdeveloper@gmail.com>
2019-05-21 17:19:39 +0000

    doc task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Doc Class Square"""
+
+
 class Square:
+    """Square Class"""
+
     def __init__(self, size):
+        """Initial size of a square
+
+           Args:
+              the size type int
+        """
         self.__size = size
