d0a3fa0582538542e8ae9b56d42abdc0a308bd11
Alexa <alexafdeveloper@gmail.com>
2019-06-04 20:25:48 +0000

    he object is exactly an instance of the specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""simple function"""
+
+def is_same_class(obj, a_class):
+    """ returns True if the object
+    is exactly an instance of the specified class"""
+    if type(obj) == a_class:
+        a = True
+    else:
+        a = False
+    return a
