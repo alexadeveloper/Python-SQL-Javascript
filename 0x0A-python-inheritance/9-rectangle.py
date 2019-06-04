fdc1c1d620de37ba25e9307ff4cb0366ef20189f
Alexa <alexafdeveloper@gmail.com>
2019-06-04 23:27:31 +0000

    adds method area and __str__

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""a class Rectangle"""
+
+
+class BaseGeometry:
+    """class with a exception"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """instantiation width, height"""
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        a = self.__width * self.__height
+        return a
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
