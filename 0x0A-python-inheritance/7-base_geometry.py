0f4444fbe0c5736acb3e0698db69274453f2481d
Alexa <alexafdeveloper@gmail.com>
2019-06-04 22:53:37 +0000

    validates the value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class simple"""
+
+
+class BaseGeometry:
+    """class with a exception"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
