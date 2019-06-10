7eef08e6d3ae5c07b94037b3cdc816042dbd519b
Alexa <alexafdeveloper@gmail.com>
2019-06-10 23:39:30 +0000

    task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,6 +12,10 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format
+                (self.id, self.__x, self.__y, self.__width, self.__height))
+
     @property
     def width(self):
         return self.__width
