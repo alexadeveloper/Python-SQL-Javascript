0fcab82f8eb3c35406aa86d5ec7799ae8b57102c
Alexa <alexafdeveloper@gmail.com>
2019-06-11 19:53:17 +0000

    comments models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""class Base"""
 
 
 class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """inicializa Base"""
+
         if id is not None:
             self.id = id
         else:
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""class Rectangle"""
+
 
 from models.base import Base
 
 
 class Rectangle(Base):
-
+    """ class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """inicializa class Rectangle"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -13,15 +16,18 @@ class Rectangle(Base):
         self.y = y
 
     def __str__(self):
+        """change str""""
         return ("[Rectangle] ({}) {}/{} - {}/{}".format
                 (self.id, self.__x, self.__y, self.__width, self.__height))
 
     @property
     def width(self):
+        """define width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """update width"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format("width"))
         if value <= 0:
@@ -30,10 +36,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """define height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """update height"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format("height"))
         if value <= 0:
@@ -42,10 +50,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """define x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """update x"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format("x"))
         if value < 0:
@@ -54,10 +64,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """define y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """update y"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format("y"))
         if value < 0:
@@ -65,9 +77,11 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """define area"""
         return self.__width * self.__height
 
     def display(self):
+        """print rectangle with #"""
         alto = self.__height
         ancho = self.__width
         for y in range(self.__y):
