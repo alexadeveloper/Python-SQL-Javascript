488795c71299fac3cc609a81ada23379cb7a8ea4
Alexa <alexafdeveloper@gmail.com>
2019-06-11 19:37:40 +0000

    tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+import unittest
+from models import base
+from models.base import Base
+Base = Base
+
+
+class TestBase(unittest.TestCase):
+
+    def test_doc(self):
+        self.assertTrue(len(base.__doc__) > 0)
+
+    def test_class_doc(self):
+        self.assertTrue(len(Base.__doc__) > 0)
+
+    def test_id(self):
+        b1 = Base(12)
+        self.assertEqual(b1.id, 12)
+
+if __name__ == '__main__':
+    unittest.main()
