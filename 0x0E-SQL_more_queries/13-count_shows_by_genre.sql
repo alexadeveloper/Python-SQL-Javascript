7365d3459bb6e1d8808c46a3f620c202027fd37d
Alexa <alexafdeveloper@gmail.com>
2019-07-16 18:55:43 +0000

    count by genre fixes genre without shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- number the show linked
 SELECT g.name as genre, COUNT(s.show_id) as number_of_shows
-FROM tv_genres g, tv_show_genres s
-WHERE s.genre_id = g.id
+FROM tv_genres g
+INNER JOIN  tv_show_genres s
+ON s.genre_id = g.id
 GROUP BY g.id
 ORDER BY COUNT(s.show_id) DESC;
