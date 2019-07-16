c1c9bb35c4631ca8f07bb4fec8956fd7a84e5d66
Alexa <47119661+alexadeveloper@users.noreply.github.com>
2019-07-16 14:08:43 -0500

    raro muy raro

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- number the show linked
 SELECT g.name as 'genre', COUNT(s.show_id) as 'number_of_shows'
 FROM tv_genres g INNER JOIN  tv_show_genres s
-ON g.id = s.genre_id
-GROUP BY g.name
-ORDER BY COUNT(s.show_id) DESC;
+    ON g.id = s.genre_id
+    GROUP BY g.name
+    ORDER BY COUNT(s.show_id) DESC;
