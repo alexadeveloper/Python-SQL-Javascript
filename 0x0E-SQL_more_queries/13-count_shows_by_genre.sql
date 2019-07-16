719b5c7ae4139af74d67ed72ad404eb7551c9a82
Alexa <alexafdeveloper@gmail.com>
2019-07-16 18:57:47 +0000

    mistery

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- number the show linked
-SELECT g.name as genre, COUNT(s.show_id) as number_of_shows
+SELECT g.name as 'genre', COUNT(s.show_id) as 'number_shows'
 FROM tv_genres g
 INNER JOIN  tv_show_genres s
 ON s.genre_id = g.id
