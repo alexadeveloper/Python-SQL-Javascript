3b48bf07f8fee059d004553becce4e0df9a823a8
Alexa <alexafdeveloper@gmail.com>
2019-08-12 16:30:54 +0000

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" list all objects state from database """
+
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    usuario = argv[1]
+    clave = argv[2]
+    basedatos = argv[3]
+    estad = argv[4]
+    motor = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                          (usuario, clave, basedatos), pool_pre_ping=True)
+    Base.metadata.create_all(motor)
+    sesion = Session(motor)
+    estado = sesion.query(State).filter(State.name == name).first()
+    if estado is not None:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+    sesion.close()
