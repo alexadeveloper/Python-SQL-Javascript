8186810a1e80439783d2445ecb1d2c5f2b34ed30
Alexa <alexafdeveloper@gmail.com>
2019-08-27 00:42:29 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" fetches https://intranet.hbtn.io/status """
+
+import requests
+
+if __name__ == '__main__':
+    url = argv[1]
+    rta = requests.get(url)
+    cabezera = rta.headers
+    if 'X-Request-Id' in cabezera:
+        print(cabezera['X-Request-Id'])
