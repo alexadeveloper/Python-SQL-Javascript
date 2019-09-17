a8ca5b2a24baccb46b5ed786aed0d52ff3f303ab
Alexa <alexafdeveloper@gmail.com>
2019-09-17 21:02:35 +0000

    print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// loop to languages
+const args = process.argv;
+const size = parseInt(args[2]);
+const value = 'X';
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+      console.log(value.repeat(size));
+  }
+}
