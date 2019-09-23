d5737cd391640876484cf11dd0c74dc79e230fdc
Alexa <alexafdeveloper@gmail.com>
2019-09-23 20:06:15 +0000

    method print

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// class rectangle
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    let str='';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        str = str + 'X';
+      }
+      str = str + '\n';
+    }
+    console.log(str.slice(0, -1));
+  }
+};
